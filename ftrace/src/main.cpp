dee66dd14f119fd02a948baf28a9301e870ef211
seb <you@example.com>
2014-03-16 22:06:47 +0100

    License info

--- ftrace/src/main.cpp
+++ ftrace/src/main.cpp
@@ -1,3 +1,26 @@
+/**
+  @file		main.cpp
+  @author	S. Besombes
+  @date		october 2011
+  @version	$Revision: 1.1.1 $
+  @brief	Time measurement functions.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston,
+   MA 02110-1301, USA.
+*/
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
